39dd14d42661f3dc9c50c8426818c92fc7ef0898
shayed <sashraf171202@bscse.uiu.ac.bd>
2022-02-11 23:46:40 +0600

    Project Added

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -13,9 +13,9 @@ const Hero = () => (
           Shayed Ashraf
 
         </SectionTitle>
-        <SectionText>To shayed work in the web development sector in a progressive organization that allows me the opportunity to update my knowledge to the latest
-          trends and be a part of a diverse and dynamic team that adds to both my personal and professional growth.</SectionText>
-        <Button onClick={() => window.location = 'https://www.google.com/'}>Learn More</Button>
+        <SectionText>I am CSE graduate from UIU. I am working with RectJS and NodeJS. I Have hands-on experience with SQA and DevOps.To  work in the Software Engineering sector in a progressive organization that allows me the opportunity to update my knowledge
+          to the latest trends and be a part of a diverse and dynamic team that adds to both my personal and professional growth.</SectionText>
+        <Button onClick={() => window.location = 'https://drive.google.com/file/d/1WgbjcCTv9NQtNXY3hNlrQr0sFKkH6aTj/view?usp=sharing'}>Download CV</Button>
       </LeftSection>
     </Section>
   </div>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -7,7 +7,7 @@ import { projects } from '../../constants/constants';
 const Projects = () => (
   <Section nopadding id="projects">
     <SectionDivider />
-    <SectionTitle main>Projectss</SectionTitle>
+    <SectionTitle main>Projects</SectionTitle>
     <GridContainer>
       {projects.map((p, i) => {
         return (
